2afa6c06f02a870687d45a3a6a2aaab096ad7609
Nicholas McEnroe <nsmcenro@gmail.com>
2023-05-31 16:42:13 -0400

    Add List of Arrays

---
+++ arrays.rb
@@ -0,0 +1,14 @@
+# List of Arrays
+
+colors = ["pink","black","red","blue"]
+numbers = [1,2,3,4]
+decimals = [0.1,0.2,0.3,0.4]
+
+daytime = true
+nighttime = false
+hungry = false
+tired = false
+
+facts = [daytime,nighttime,hungry,tired]
+
+
